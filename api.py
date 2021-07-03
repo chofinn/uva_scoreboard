061133899521f614118074a351e53abfbf2819e2
chofinn <chofinn@gmail.com>
2021-07-03 23:45:02 +0900

    add get_info

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 import re
 import json
 from datetime import datetime
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
@@ -16,6 +17,45 @@ ma = Marshmallow(app)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+status_code = {
+    10 : "Submission error",
+    15 : "Can't be judged",
+    20 : 'In queue',
+    30 : 'Compile error',
+    35 : 'Restricted function',
+    40 : 'Runtime error',
+    45 : 'Output limit',
+    50 : 'Time limit',
+    60 : 'Memory limit',
+    70 : 'Wrong answer',
+    80 : 'PresentationE',
+    90 : 'Accepted',
+}
+def get_info(user_name):
+    uva_id = str(json.loads(urlopen('https://uhunt.onlinejudge.org/api/uname2uid/' + user_name).read()))
+    response = urlopen('https://uhunt.onlinejudge.org/api/subs-user/'+ uva_id).read()
+
+    results = json.loads(response).get("subs")
+    ac = 0
+    for r in results:
+        col_values = []
+
+        probs = json.loads(urlopen('https://uhunt.onlinejudge.org/api/p/id/'+ str(r[1])).read())
+        col_values.append(probs['num'])
+        col_values.append(probs['title'])
+        col_values.append(status_code[r[2]])
+        col_values.append(datetime.utcfromtimestamp(r[4]))
+        col_values.append(r[3])
+        col_values.append(user_name)
+        col_values.append('UVa')
+        if(r[2] == 90):
+            ac += 1
+
+        new_quest = Quest(*col_values)
+        db.session.add(new_quest)
+        db.session.commit()
+
+    return ac, len(results)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #           Tables          #
@@ -23,14 +63,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 class Quest(db.Model):
     quest_id = db.Column(db.Integer, primary_key=True)
     qid = db.Column(db.Integer)
+    quest_name = db.Column(db.String(100))
     status = db.Column(db.String(100))
     submit_time = db.Column(db.DateTime)
     run_time = db.Column(db.Float)
     submitter = db.Column(db.String(50))
     platform = db.Column(db.String(100))
     
-    def __init__(self, qid, status, submit_time, run_time, submitter, platform ):
+    def __init__(self, qid, quest_name, status, submit_time, run_time, submitter, platform ):
         self.qid = qid
+        self.quest_name = quest_name
         self.status = status
         self.submit_time = submit_time
         self.run_time = run_time
@@ -58,7 +100,7 @@ class User(db.Model):
 class QuestSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('qid', 'status', 'submit_time', 'run_time', 'submitter', 'platform')
+        fields = ('qid', 'quest_name', 'status', 'submit_time', 'run_time', 'submitter', 'platform')
 
 class UserSchema(ma.Schema):
     class Meta:
@@ -209,8 +251,15 @@ def user_delete(id):
 
     return {'message': 'successfully delete user'}, 200
 
-
+@app.route("fetch_data", methods=["POST"])
+def fetch_data():
+    name = request.values['user_name']
+    user = User.query.filter_by(uid=name).first()
+    ac, total = get_info(getattr(user, 'user_name'))
+    setattr(user, 'ac', ac)
+    setattr(user, 'total', total)
+    db.session.commit()
 
 if __name__ == '__main__':
-    app.run(debug = True, host="0.0.0.0", port=20002)
+    #app.run(debug = True, host="0.0.0.0", port=20002)
     app.run(debug=True)
